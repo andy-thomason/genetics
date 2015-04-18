4d07bc1504da8373b84f589ee973d2bac900ef64
andy_thomason <andy@andythomason.com>
2015-04-18 21:52:19 +0100

    substr to match string

--- include/boost/genetics/genetics.hpp
+++ include/boost/genetics/genetics.hpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <cstdlib>
-#include "stdint.h"
 
 #include <string>
 #include <iosfwd>
 #include <type_traits>
+#include <vector>
+#include <algorithm>
 
 namespace boost { namespace genetics {
+    typedef unsigned char uint8_t;
+    typedef unsigned short uint16_t;
+    typedef unsigned int uint32_t;
+    typedef unsigned long long uint64_t;
+    typedef long long int64_t;
+
     static inline bool is_base(unsigned int chr) {
         static const uint64_t vals = (
             (1 << ('A'-'A')) |
@@ -28,30 +35,26 @@ namespace boost { namespace genetics {
         return idx <= 'T'-'C' ? (int)(vals >> (idx*2)) & 0x03 : 0x00;
     }
 
-    static inline unsigned int code_to_base(int code) {
-        return (('A' | 'C' << 8 | 'G' << 16 | 'T' << 24) >> (code * 8)) & 0xff;
+    static inline char code_to_base(int code) {
+        return (char)((('A' | 'C' << 8 | 'G' << 16 | 'T' << 24) >> (code * 8)));
     }
 
-    /// fixed length vector of packed bases
-    template <size_t N> class bases {
+    /// variable length vector of packed bases
+    class bases {
     public:
-        typedef size_t is_bases;
-        static const size_t num_bases = N;
-        typedef unsigned long long word_type;
+        typedef uint64_t word_type;
         static const size_t bases_per_value = sizeof(word_type) * 4;
-        static const size_t num_values = (num_bases + bases_per_value-1) / bases_per_value;
 
     public:
-        bases(unsigned long long val = 0) {
-            values[0] = val;
-            for (size_t i = 1; i < num_values; ++i) {
-                values[i] = 0;
-            }
+        bases(size_t size=0) {
+            num_bases = size;
+            values.resize(num_bases * bases_per_value);
         }
 
         template<class InIter>
         bases(InIter b, InIter e) {
-            init(b, e);
+            num_bases = 0;
+            append(b, e);
         }
 
         template<class String>
@@ -59,7 +62,8 @@ namespace boost { namespace genetics {
             const String &str,
             size_t pos = 0, size_t n = ~(size_t)0
         ) {
-            init(str.begin() + pos, str.begin() + std::min(n, str.size()));
+            num_bases = 0;
+            append(str.begin() + pos, str.begin() + std::min(n, str.size()));
         }
 
         template <class charT>
@@ -67,129 +71,250 @@ namespace boost { namespace genetics {
             const charT* str,
             size_t pos = 0, size_t n = ~(size_t)0
         ) {
+            num_bases = 0;
             const charT *b = str + pos;
             const charT *e = str + pos;
             while (e - b != n && *e) ++e;
-            init(b, e);
+            append(b, e);
         }
 
-        unsigned long long to_ullong() const {
-            return values[0];
-        }
-
-        unsigned char operator[](size_t index) const {
+        char operator[](size_t index) const {
             size_t sh = ((bases_per_value - 1 - index) % bases_per_value) * 2;
             size_t off = index/bases_per_value;
-            return (unsigned char)((values[off] >> sh) & 0x03);
+            return index >= num_bases ? 'N' : code_to_base((values[off] >> sh) & 0x03);
         }
 
-        template<class String=std::string>
-        String to_string() const {
-            String res;
+        operator std::string() const {
+            std::string res;
             res.resize(num_bases);
             for (size_t i = 0; i != num_bases; ++i) {
-                res[i] = code_to_base((*this)[i]);
+                res[i] = (*this)[i];
             }
             return res;
         }
 
-        template <class Type>
-        void rev_comp(Type &result, size_t offset=0, size_t length=~(size_t)0) const {
-            length = std::min(length, result.size());
+        bases substr(size_t offset=0, size_t length=~(size_t)0, bool rev_comp=false) const {
+            bases result;
             length = std::min(length, size() - offset);
             size_t nv = (length + bases_per_value - 1) / bases_per_value;
-            for (size_t i = 0; i < num_values; ++i) {
-                word_type word = window((ptrdiff_t)(num_bases - bases_per_value - i * bases_per_value));
-                result.values[i] = rev_comp_word(word);
+            result.num_bases = length;
+            result.values.resize(nv);
+            if (!rev_comp) {
+              for (size_t i = 0; i < nv; ++i) {
+                  result.values[i] = window((int64_t)(offset + i * bases_per_value));
+              }
+            } else {
+              for (size_t i = 0; i < nv; ++i) {
+                  result.values[i] = rev_comp_word(
+                    window((int64_t)(offset + length - (i+1) * bases_per_value))
+                  );
+              }
             }
-        }
-
-        template <class Type>
-        void substring(Type &result, size_t offset=0, size_t length=~(size_t)0) const {
-            size_t nv = (length + bases_per_value - 1) / bases_per_value;
-            for (size_t i = 0; i < nv; ++i) {
-                result.values[i] = window((ptrdiff_t)(offset + i * bases_per_value));
+            if (result.num_bases % bases_per_value) {
+                result.values[nv-1] &= ~(word_type)0 << (((0-length) % bases_per_value) * 2);
             }
+            return result;
         }
 
         size_t distance(const bases &rhs) const {
             size_t diff = 0;
-            for (size_t i = 0; i < num_values; ++i) {
+            for (size_t i = 0; i < values.size(); ++i) {
                 diff += count_word(values[i] ^ rhs.values[i]);
             }
             return diff;
         }
 
         size_t size() const {
-          return num_bases;
+            return num_bases;
         }
-    private:
+
+        void reserve(size_t size) {
+            values.reserve((size + bases_per_value - 1) / bases_per_value);
+        }
+
+        void resize(size_t size) {
+            values.resize((size + bases_per_value - 1) / bases_per_value);
+            if (size < num_bases && size % bases_per_value) {
+                size_t last = size / bases_per_value;
+                values[last] &= ~(word_type)0 << (((0-size) % bases_per_value) * 2);
+            }
+            num_bases = size;
+        }
+
+        void append(const char *str) {
+            const char *e = str;
+            while (*e) ++e;
+            append(str, e);
+        }
+
         template<class InIter>
-        void init(InIter b, InIter e) {
-            size_t i = 0;
-            size_t d = 0;
-            word_type acc = 0;
-            for (; b != e && i != num_bases; ++i) {
+        void append(InIter b, InIter e) {
+            size_t max_bases = values.size() * bases_per_value;
+            word_type acc = num_bases < max_bases ? values.back() >> (max_bases - num_bases) * 2 : 0;
+            while (b != e) {
                 acc = acc * 4 + base_to_code((int)*b++);
-                if ((i+1) % bases_per_value == 0) {
-                    values[d++] = acc;
+                num_bases++;
+                if (num_bases % bases_per_value == 0) {
+                    max_bases = values.size() * bases_per_value;
+                    if (num_bases > max_bases) {
+                        values.push_back(acc);
+                    } else {
+                        values.back() = acc;
+                    }
                     acc = 0;
                 }
             }
-            if (i % bases_per_value && d != num_values) {
-                acc <<= (bases_per_value - i % bases_per_value) * 2;
-                values[d++] = acc;
-            }
-            while (d < num_values) {
-                values[d++] = 0;
+
+            max_bases = values.size() * bases_per_value;
+            acc <<= (max_bases - num_bases) * 2;
+            if (num_bases > max_bases) {
+                values.push_back(acc);
+            } else {
+                values.back() = acc;
             }
         }
+
+        bool operator==(const bases &rhs) const {
+            return values == rhs.values && size() == rhs.size();
+        }
+
+        bool operator!=(const bases &rhs) const {
+            return values != rhs.values || size() != rhs.size();
+        }
+
+        bool operator>(const bases &rhs) const {
+            return values > rhs.values || values == rhs.values && size() > rhs.size();
+        }
+
+        bool operator<(const bases &rhs) const {
+            return values < rhs.values || values == rhs.values && size() < rhs.size();
+        }
+
+        bool operator>=(const bases &rhs) const {
+            return values > rhs.values || values == rhs.values && size() >= rhs.size();
+        }
+
+        bool operator<=(const bases &rhs) const {
+            return values < rhs.values || values == rhs.values && size() <= rhs.size();
+        }
+
     private:
-        word_type window(ptrdiff_t base) const {
-          word_type v0 = base < 0 ? 0 : values[base/bases_per_value];
-          if (base % bases_per_value) {
-            return v0;
-          } else {
-            word_type v1 = base/bases_per_value+1 < num_values ? values[base/bases_per_value+1] : 0;
-            return v0 << (base % bases_per_value) | v1 >> ((0-base) % bases_per_value);
-          }
+        word_type window(int64_t base) const {
+            word_type v0 = base < 0 ? 0 : values[base/bases_per_value];
+            if (base % bases_per_value == 0) {
+                return v0;
+            } else {
+                word_type v1 = base/bases_per_value+1 < values.size() ? values[base/bases_per_value+1] : 0;
+                return v0 << ((base % bases_per_value)*2) | v1 >> (((0-base) % bases_per_value)*2);
+            }
         }
 
         static word_type rev_comp_word(word_type x) {
-          x = _byteswap_uint64(x);
-          x = ((x & 0x3333333333333333) << 2) | ((x >> 2) & 0x3333333333333333);
-          return ~x;
+            x = _byteswap_uint64(x);
+            x = ((x & 0x3333333333333333) << 2) | ((x >> 2) & 0x3333333333333333);
+            return ~x;
         }
 
         static size_t count_word(word_type x) {
-          x |= x >> 1;
-          x &= 0x5555555555555555;
-          return (size_t)__popcnt64(x);
+            x |= x >> 1;
+            x &= 0x5555555555555555;
+            return (size_t)__popcnt64(x);
         }
 
-        unsigned long long values[num_values];
+        size_t num_bases;
+        std::vector<word_type> values;
     };
 
-    template <class charT, class traits, size_t N>
+    class bases_and_letters : public bases {
+        static const size_t lg_bases_per_index = 16;
+        static const size_t bases_per_index = (size_t)1 << lg_bases_per_index;
+        typedef uint32_t index_type;
+        typedef uint32_t rle_type;
+
+        // todo: introduce special codes for common cases
+        //static const index_type empty_value = (index_type)-1;
+        //static const index_type n_value = (index_type)-2;
+    public:
+        bases_and_letters() {
+        }
+
+        bases_and_letters(const char *str) {
+            const char *e = str;
+            while (*e) ++e;
+            append(str, e);
+        }
+
+        char operator[](int64_t base) const {
+            if ((uint64_t)base/bases_per_index > index.size()) return 'N';
+
+            index_type i = index[(uint64_t)base/bases_per_index];
+            //if (i == empty_value) return (*(const bases*)this)[base];
+            //if (i == n_value) return 'N';
+
+            index_type i1 =
+              (uint64_t)base/bases_per_index == index.size() ?
+              (index_type)rle.size() :
+              index[(uint64_t)base/bases_per_index+1]
+            ;
+
+            rle_type search = (rle_type)((uint64_t)(base % bases_per_index) << 8);
+            const rle_type *b = rle.data() + i;
+            const rle_type *e = rle.data() + i1;
+            const rle_type *p = std::lower_bound(b, e, search);
+            return (*p & 0xff) ? (*p & 0xff) : (*(const bases*)this)[base];
+        }
+    private:
+        void resize(size_t size); // = deleted
+
+        template<class InIter>
+        void append(InIter b, InIter e) {
+          size_t num_bases = size();
+          bases::append(b, e);
+          rle_type back = rle.back();
+          while (b != e) {
+            int chr = (int)*b++;
+            int val = (is_base(chr)) ? 0 : chr;
+            if (num_bases % bases_per_index == 0 || (back & 0xff) != val) {
+              back = (rle_type)((num_bases % bases_per_index) * 256 + chr);
+              rle.push_back(back);
+            }
+          }
+        }
+
+
+        std::vector<index_type> index;
+        std::vector<rle_type> rle;
+    };
+
+    class annotation {
+    public:
+    private:
+        typedef uint32_t index_type;
+        std::vector<index_type> index;
+        std::vector<uint8_t> data;
+    };
+
+    template <class charT, class traits>
     std::basic_istream<charT, traits>&
-    operator>>(std::basic_istream<charT, traits>& is, bases<N>& x) {
+    operator>>(std::basic_istream<charT, traits>& is, bases& x) {
         std::string str;
         is >> str;
-        x = bases<N>(str);
+        x = bases(str);
         return is;
     }
 
-    template <class charT, class traits, size_t N>
+    template <class charT, class traits>
     std::basic_ostream<charT, traits>&
-    operator<<(std::basic_ostream<charT, traits>& os, const bases<N>& x) {
-        os << x.to_string();
+    operator<<(std::basic_ostream<charT, traits>& os, const bases& x) {
+        os << (std::string)x;
         return os;
     }
 
     template <class Type>
     Type rev_comp(const Type &x, typename Type::word_type *y = 0) {
       Type result;
-      x.rev_comp(result);
+      result.resize(x.size());
+      x.substring(result, 0, x.size(), true);
       return result;
     }
 
--- test/packed_test.cpp
+++ test/packed_test.cpp
@@ -7,73 +7,110 @@
 #define BOOST_TEST_MODULE Genetics
 #include <boost/test/unit_test.hpp>
 
-
-void test_bases()
+BOOST_AUTO_TEST_CASE( test_bases )
 {
     using namespace boost::genetics;
 
     {
-        bases<8> b("ACGT");
+        bases b("ACGT");
         BOOST_MESSAGE( b );
-        BOOST_CHECK( b.to_ullong() == 0x1b00000000000000ull );
-        BOOST_CHECK( b.to_string() == "ACGTAAAA" );
+        BOOST_CHECK( b == "ACGT" );
+        b.append("TTGA");
+        BOOST_MESSAGE( b );
+        BOOST_CHECK( b == "ACGTTTGA" );
+        b.append("ACCGACCG" "ACCGACCG" "TGCGACCG" "ACCGACCG");
+        BOOST_MESSAGE( b );
+        BOOST_CHECK( b == "ACGTTTGA" "ACCGACCG" "ACCGACCG" "TGCGACCG" "ACCGACCG" );
+        BOOST_CHECK( b[-1] == 'N' );
+        BOOST_CHECK( b[0] == 'A' );
+        BOOST_CHECK( b[31] == 'G' );
+        BOOST_CHECK( b[32] == 'A' );
+        BOOST_CHECK( b[33] == 'C' );
+        BOOST_CHECK( b[999] == 'N' );
     }
 
     {
-        bases<4> b("TGCAACACACAC");
-        BOOST_CHECK( b.to_ullong() == 0xe400000000000000ull );
-        BOOST_CHECK( b.to_string() == "TGCA" );
+        bases b("ACGT");
+        b.resize(3);
+        b.resize(4);
+        BOOST_MESSAGE( b );
+        BOOST_CHECK( b == "ACGA" );
     }
 
     {
-        bases<100> b("TGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACACCCCCCCCCCCCC");
-        //BOOST_MESSAGE( b );
-        BOOST_CHECK( b.to_string() == "TGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACAT");
+        bases b("TGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACAT");
+        BOOST_MESSAGE( b );
+        BOOST_CHECK( b == "TGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACATGCAACACACAT");
     }
 
     {
-        std::string str("tTAACcGGTTACGTACGTACGTACGTACGTACGTACGT");
-        bases<64> b( str );
-        BOOST_CHECK( b.to_ullong() == 0xf05af1b1b1b1b1b1ull );
+        std::string str("tTAACcGGTTACgtacgtacGTACGTACGTACGTACGT");
+        bases b( str );
+        BOOST_CHECK( b == "TTAACCGGTTACGTACGTACGTACGTACGTACGTACGT" );
     }
 
     {
-        bases<16> b;
+        bases b;
         std::stringstream ss("TGCAACGTttttXXXX\nAACCGGTT");
         ss >> b;
         //BOOST_MESSAGE( b );
-        BOOST_CHECK( b.to_string() == "TGCAACGTTTTTAAAA" );
+        BOOST_CHECK( b == "TGCAACGTTTTTAAAA" );
         ss >> b;
         BOOST_MESSAGE( b );
-        BOOST_CHECK( b.to_string() == "AACCGGTTAAAAAAAA" );
+        BOOST_CHECK( b == "AACCGGTT" );
     }
 
     {
-        bases<32> b("ACGTACGTACGT");
+        bases b("ACGTACGTACGT");
         char buf[64] = {0};
         std::stringstream ss(buf);
         ss << b;
         BOOST_MESSAGE( std::string(ss.str()) );
-        BOOST_CHECK( std::string(ss.str()) == "ACGTACGTACGTAAAAAAAAAAAAAAAAAAAA" );
+        BOOST_CHECK( std::string(ss.str()) == "ACGTACGTACGT" );
     }
+
 }
 
-void test_algorithms()
+/*BOOST_AUTO_TEST_CASE( rev_comp )
 {
     using namespace boost::genetics;
 
     {
-        bases<11> b("TGCAACACACA");
-        BOOST_CHECK( rev_comp(b).to_string() == "TGTGTGTTGCA" );
+        bases b("TGCAACACACA");
+        BOOST_MESSAGE( rev_comp(b) );
+        BOOST_CHECK( rev_comp(b) == "TGTGTGTTGCA" );
     }
     {
         std::string b("TGNAACACACA");
         BOOST_CHECK( rev_comp(b) == "TGTGTGTTNCA" );
     }
+}*/
+
+BOOST_AUTO_TEST_CASE( substr )
+{
+    using namespace boost::genetics;
+
+    {
+        bases b("ACGTTTGA" "ACCGACCG" "ACCGACCG" "TGCGACCG" "ACCGACCG");
+        bases c;
+        c = b.substr(8, 8);
+        BOOST_MESSAGE( c );
+        BOOST_CHECK( c == "ACCGACCG" );
+        c = b.substr(8, 40);
+        BOOST_MESSAGE( c );
+        BOOST_CHECK( c == "ACCGACCG" "ACCGACCG" "TGCGACCG" "ACCGACCG");
+    }
+
+    {
+        std::string b("ACGTTTGA" "ACCGACCG" "ACCGACCG" "TGCGACCG" "ACCGACCG");
+        std::string c;
+        c = b.substr(8, 8);
+        BOOST_MESSAGE( c );
+        BOOST_CHECK( c == "ACCGACCG" );
+        c = b.substr(8, 40);
+        BOOST_MESSAGE( c );
+        BOOST_CHECK( c == "ACCGACCG" "ACCGACCG" "TGCGACCG" "ACCGACCG");
+    }
+
 }
 
-BOOST_AUTO_TEST_CASE( test_all )
-{
-    test_bases();
-    test_algorithms();
-}
