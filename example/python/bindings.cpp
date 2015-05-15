a3fda75ddfdd6e1aa677a1b0dc4734095c01abdf
Riley Doyle <rileyd@desktopgenetics.com>
2015-05-15 12:45:12 +0100

    fix file location issue

--- example/python/bindings.cpp
+++ example/python/bindings.cpp
@@ -5,20 +5,14 @@
 using namespace boost::python;
 
 class Fasta {
-    //typedef boost::genetics::dna_string dna_string; 
+    //typedef boost::genetics::dna_string dna_string;
     typedef boost::genetics::augmented_string augmented_string;
-    boost::genetics::fasta_file fasta; 
+    boost::genetics::fasta_file fasta;
 public:
     Fasta() {
     }
 
-<<<<<<< HEAD
     Fasta(const std::string &filename) : fasta(filename) {
-=======
-    void load(const std::string &filename)
-    {
-        fasta = boost::genetics::fasta_file(filename);
->>>>>>> 63ca159381204163430aed01d101db82f5d3a40e
     }
 
     ~Fasta() {
@@ -40,10 +34,10 @@ public:
 
     list get_chromosomes() const {
         list result;
-        
+
         return result;
     }
-}; 
+};
 
 BOOST_PYTHON_MODULE(genetics)
 {
