68e9393a7ebd61a8d17f806eeb00f49e9fa43fb9
andy_thomason <andy@andythomason.com>
2015-05-18 00:06:18 +0100

    fix build.bat and add env.bat for windows in lie of propper bjam build

--- example/python/build.bat
+++ example/python/build.bat
@@ -1,1 +1,1 @@
-cl /EHsc /LD /DBOOST_ALL_NO_LIB=1 /I ..\..\..\.. /I c:\Python27\include c:\Python27\libs\python27.lib bindings.cpp ..\..\..\..\lib64-msvc-12.0\boost_python-vc120-mt-1_57.lib /Fegenetics.pyd 
+cl /MD /EHsc /LD /DBOOST_ALL_NO_LIB=1 /I ..\..\..\.. /I c:\Python27\include c:\Python27\libs\python27.lib bindings.cpp ..\..\..\..\lib64-msvc-12.0\boost_python-vc120-mt-1_57.lib /Fegenetics.pyd 
