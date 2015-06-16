fed1acd61323e1e486eb38c74dde2ced63835903
Andy Thomason <andy@andythomason.com>
2015-06-16 20:15:07 +0100

    add copyright messages

--- include/boost/genetics/utils.hpp
+++ include/boost/genetics/utils.hpp
@@ -1,3 +1,8 @@
+// Copyright Andy Thomason 2015
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef BOOST_GENETICS_UTILS_HPP
 #define BOOST_GENETICS_UTILS_HPP
 
